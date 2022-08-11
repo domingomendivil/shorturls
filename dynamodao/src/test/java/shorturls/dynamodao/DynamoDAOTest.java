4530b12972676184e3d46efd14a531ea73ffb4c5
John Doe <johndoe@example.com>
2022-08-10 21:06:51 -0300

    commit

--- dynamodao/src/test/java/shorturls/dynamodao/DynamoDAOTest.java
+++ dynamodao/src/test/java/shorturls/dynamodao/DynamoDAOTest.java
@@ -1,34 +1,20 @@
 package shorturls.dynamodao;
 
-import static org.mockito.Mockito.when;
-import static software.amazon.awssdk.services.dynamodb.model.AttributeValue.fromS;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import lombok.val;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
-import software.amazon.awssdk.services.dynamodb.model.DeleteItemResponse;
 
 public class DynamoDAOTest {
 
-    @InjectMocks
+   // @InjectMocks
     private DynamoDAO dynamoDAO;
 
-    @Mock
+    //@Mock
     private DynamoDbClient client;
 
     public void test1(){
 
     }
-    @Test
-    /**public void testDeleteById1(){
+    /**@Test
+    public void testDeleteById1(){
         val key = new HashMap<String, AttributeValue> ();
         key.put("shortPath", fromS("a"));
         val req = DeleteItemRequest.builder()
@@ -39,6 +25,6 @@ public class DynamoDAOTest {
         dynamoDAO.deleteById("a");
     }**/
 
-    
+
 
 }
