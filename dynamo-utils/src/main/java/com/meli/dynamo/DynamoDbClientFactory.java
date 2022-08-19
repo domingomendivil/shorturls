3abed0443503781524f39400e28347016e677727
John Doe <johndoe@example.com>
2022-08-19 00:31:26 -0300

    mejoras

--- dynamo-utils/src/main/java/com/meli/dynamo/DynamoDbClientFactory.java
+++ dynamo-utils/src/main/java/com/meli/dynamo/DynamoDbClientFactory.java
@@ -7,21 +7,28 @@ import lombok.val;
 import shorturls.config.ShortURLProperties;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 
+/**
+ * Factory class for getting an instance of DynamoDbAsyncClient
+ */
 public class DynamoDbClientFactory {
 
 	private DynamoDbClientFactory() {
-		
+		//no public constructor.
+        //class cannot be instantiated
 	}
 	
 	@Getter(lazy=true)
 	private static final DynamoDbAsyncClient client = getClient(new ShortURLProperties());
 	
+    /*
+     * Creates a new instance of DynamoDbAsynClient
+     */
 	private static final DynamoDbAsyncClient getClient(ShortURLProperties props) {
 		val dynamoURL = props.getProperty("DYNAMO_URL");
         DynamoDbAsyncClient client;
         if ((dynamoURL==null) || (dynamoURL.equals(""))){
             client=  DynamoDbAsyncClient.create();  //If no URL is provided, it's implicitly obtained from AWS 
-        }else {  
+        }else {   //otherwise, the URL provided is used
             client = DynamoDbAsyncClient
             .builder().
             endpointOverride(URI.create(dynamoURL)).build();
--- get-urls-common/src/test/java/geturl/config/ServiceFactoryTest.java
+++ get-urls-common/src/test/java/geturl/config/ServiceFactoryTest.java
@@ -20,29 +20,11 @@ public class ServiceFactoryTest {
 	@Mock
 	private ShortURLProperties properties;
 	
-/**	@Test
-	public void test0() {
-		when(properties.getProperty("DYNAMO_URL")).thenReturn("http://dynamo-local:8000");
-		when(properties.getProperty(Constants.CACHE_ENABLED)).thenReturn("false");
-		when(properties.getProperty(Constants.QUERY_FACTORY)).thenReturn("shorturls.dynamodao.DynamoDAOFactory");
-		when(properties.getProperty(Constants.BASE_URL)).thenReturn("http://127.0.0.1:3000");
-		when(properties.getProperty(Constants.EVENTS_FACTORY)).thenReturn("com.meli.kafka.KafkaFactory");
-		when(properties.getProperty(Constants.RANDOM_ALPHABET)).thenReturn("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-		when(properties.getProperty("KAFKA_ENABLED")).thenReturn("false");
-
-		ServiceFactory.init(properties,new Factory<Query>(),new Factory<Cache>(),new Factory<Events>());
-	}**/
 	
 	@Test(expected=ConfigurationException.class)
 	public void test1() {
 		ConfigurationException e = new ConfigurationException("");
 		when(properties.getProperty("CACHE_ENABLED")).thenThrow(e );
-
-		/**
-		Factory factory1 = Mockito.mock(Factory.class);
-		Factory factory2 = Mockito.mock(Factory.class);
-		Factory factory3 = Mockito.mock(Factory.class);
-**/
 		ServiceFactory.init(properties,null,null,null);
 	}
 	
@@ -67,23 +49,6 @@ public class ServiceFactoryTest {
 		when(factory.getInstance("badFactoryClass")).thenThrow(e);
 		ServiceFactory.init(properties,factory,null,null);
 	}
-	
-	
-	/*@Test(expected=ConfigurationException.class)
-	public void test4() {
-		when(properties.getProperty(Constants.CACHE_ENABLED)).thenReturn("true");
-		when(properties.getProperty(Constants.QUERY_FACTORY)).thenReturn("shorturls.dynamodao.DynamoDAOFactory");
-		when(properties.getProperty(Constants.CACHE_FACTORY)).thenReturn("badFactoryClass");
-		
-		ServiceFactory.init(properties);
-	}
 
-	@Test(expected=ConfigurationException.class)
-	public void test5() {
-		when(properties.getProperty(Constants.CACHE_ENABLED)).thenReturn("true");
-		when(properties.getProperty(Constants.QUERY_FACTORY)).thenReturn("shorturls.dynamo.DynamoDAOFactory");
-		when(properties.getProperty(Constants.CACHE_FACTORY)).thenReturn("shorturl.cache.RedisCacheFactory");
-		ServiceFactory.init(properties);
-	}**/
 
 }
