f064c2f53eb01e13c11d1eb1eb275b9035d8f749
John Doe <johndoe@example.com>
2022-08-30 19:12:01 -0300

    mejoras

--- shorturl-utils/src/main/java/shorturls/apigateway/PunyCodeConverter.java
+++ shorturl-utils/src/main/java/shorturls/apigateway/PunyCodeConverter.java
@@ -6,10 +6,15 @@ import java.util.StringJoiner;
 
 import lombok.val;
 
+/**
+ * Utility class for encoding URLs to punycode.
+ * 
+ */
 public class PunyCodeConverter {
 	
 	private static final String POINT =".";
 	private static final String SLASH ="/";
+	private static final String POINT_DOUBLE_SLASH ="://";
 	
 	/**
 	 * Gets the original URL and transform it to a punycode one.
@@ -21,23 +26,36 @@ public class PunyCodeConverter {
 	 * to ASCII ones.
 	 * Examples of URLs with special characters are:
 	 * http://www.peñarol.org.
-	 * This URL will be converted to http://www.xn--pearol-xwa.org/ 
+	 * This URL will be converted to http://www.xn--pearol-xwa.org/.
+	 * In case that the URL to convert is invalid, it throws a MalformedURLException
 	 * @param url The original URL to punycode.
 	 * @return the punycoded URL
-	 * @throws MalformedURLException
+	 * @throws MalformedURLException the exception thrown in case that the URL to convert is invalid
 	 */
 	public static String toPunycode(String url) throws MalformedURLException {
-		URL u = new URL(url);
+		val u = new URL(url);
 		val punicoded =getPunicoded(u.getHost(),POINT);
 		val pathPunicoded = getPunicoded(u.getPath(),SLASH);
-		return u.getProtocol()+"://"+punicoded+pathPunicoded;
+		val sb = new StringBuffer();
+		sb.append(u.getProtocol()).append(POINT_DOUBLE_SLASH).append(punicoded).append(pathPunicoded);
+		return sb.toString();
 	}
 	
-	private static String getPunicoded(String url,String separator) {
-		String[] labels = url.split("\\"+separator);
+	/**
+	 * Method to convert to punycode each part of the URL.
+	 * Basically the two parts to convert are the domain (base part)
+	 * and the rest of the path.
+	 * E.g. in http://www.peñarol.org/a/index.html
+	 * the domain part is www.peñarol.org
+	 * @param string The string to convert to punycode
+	 * @param separator The separator part, in case of the base part is the point "."
+	 * @return the punycode converted string
+	 */
+	private static String getPunicoded(String string,String separator) {
+		val labels = string.split("\\"+separator);
 		val joiner = new StringJoiner(separator);
 		for (int i=0;i<labels.length;i++) {
-			String str= java.net.IDN.toASCII(labels[i]);
+			val str= java.net.IDN.toASCII(labels[i]);
 			joiner.add(str);
 		}
 		return joiner.toString();
